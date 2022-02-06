a76c5ba40de87a949d48589dcb3712761b94257b
Nikita Sachivko <nsachivko@myseneca.ca>
2022-02-05 21:32:55 -0500

    404 fix

--- src/components/Navigation/Bar.js
+++ src/components/Navigation/Bar.js
@@ -3,6 +3,7 @@ import * as React from "react"
 
 
 const Bar = ({ routes }) => {
+    routes = routes.length === 0 ? [{}] : routes
     return (
         <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,13 +6,17 @@ import Seo from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
+  const getRoutes = require("../components/Routes/GetRoutes")
+  const routes = getRoutes.default()
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    <>
+      <Layout location={location} title={siteTitle} routes={routes}>
+        <Seo title="404: Not Found" />
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    </>
   )
 }
 
